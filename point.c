cb08c5cf7892cb0ccd9a3fe9600eaef84200e5f2
lvcky-gg <matadorcrab@gmail.com>
2024-02-05 19:46:40 -0500

    pointers

--- point.c
+++ point.c
@@ -3,8 +3,14 @@ int main() {
   int a = 5;
   int *p;
   p = &a;
-  printf("%p", &a);
-  printf("p", p);
+  printf("%p\n", &a);
+  printf("%p\n", p);
+
+  int b;
+  int *k;
+  k = &b;
+  *k = 5;
+  printf("%i\n", &k);
 
   return 0;
 }
